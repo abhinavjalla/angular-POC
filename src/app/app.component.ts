7d95880669f349fcf154d620570e889b39cb5ad9
abhinavjalla <abhi.jalla@gmail.com>
2019-06-20 23:11:14 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,18 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  implements OnInit {
-   loginForm: FormGroup;
+   myform: FormGroup;
   ngOnInit() {
     this.createForm();
   }
 
   private createForm() {
-    this.loginForm = new FormGroup({
-      // tslint:disable-next-line
-      email: new FormControl('', [Validators.required, patternValidator(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
+    this.myform = new FormGroup({
+  
+      name: new FormControl('', [Validators.required,Validators.minLength(3)]),
       password: new FormControl('', Validators.required),
     });
   }
 
-  name = 'Angular';
+
 }
