c89ce9fb5a10527f1883ce2573fba368adb4fb65
abhinavjalla <abhi.jalla@gmail.com>
2019-06-20 19:26:29 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent  implements OnInit {
+   loginForm: FormGroup;
+  ngOnInit() {
+    this.createForm();
+  }
+
+  private createForm() {
+    this.loginForm = new FormGroup({
+      // tslint:disable-next-line
+      email: new FormControl('', [Validators.required, patternValidator(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
   name = 'Angular';
 }
