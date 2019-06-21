3be96a8966db3b7e0c823e57eb2c6380e941228e
abhinavjalla <abhi.jalla@gmail.com>
2019-06-21 11:20:38 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,12 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  implements OnInit {
+
+
    myform: FormGroup;
+   emailpt = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+   phnpt =/^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$/;
+   
   ngOnInit() {
     this.createForm();
   }
@@ -16,9 +21,14 @@ export class AppComponent  implements OnInit {
     this.myform = new FormGroup({
   
       name: new FormControl('', [Validators.required,Validators.minLength(3)]),
-      password: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required,Validators.pattern(this.emailpt)]),
+      phone: new FormControl('', [Validators.required,Validators.pattern(this.phnpt)]),
+      comment:new FormControl('',[Validators.required,Validators.minLength(15)])
     });
   }
 
 
 }
+// ,Validators.pattern('^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$')
+
+//,Validators.pattern(this.pt)
